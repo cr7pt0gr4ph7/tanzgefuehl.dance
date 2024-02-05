d358f65046f313e859e6086b76c8af74ce7a766c
Lukas Waslowski <cr7pt0gr4ph7@googlemail.com>
2024-02-05 15:33:57 +0000

    Add pluralize_word to _plugins/pluralize.rb

--- _plugins/pluralize.rb
+++ _plugins/pluralize.rb
@@ -2,20 +2,31 @@
 #
 # A Liquid filter to make it easy to form correct plurals.
 #
-# https://github.com/bdesham/pluralize
+# Based on https://github.com/bdesham/pluralize
 
 module Pluralize
 
-	def pluralize(number, singular, plural = nil)
-		number = number.to_i
-		if number == 1
-			"#{number} #{singular}"
-		elsif plural.nil?
-			"#{number} #{singular}s"
-		else
-			"#{number} #{plural}"
-		end
-	end
+    def pluralize(number, singular, plural = nil)
+        number = number.to_i
+        if number == 1
+            "#{number} #{singular}"
+        elsif plural.nil?
+            "#{number} #{singular}s"
+        else
+            "#{number} #{plural}"
+        end
+    end
+
+    def pluralize_word(number, singular, plural = nil)
+        number = number.to_i
+        if number == 1
+            singular.to_s
+        elsif plural.nil?
+            "#{singular}s"
+        else
+            plural.to_s
+        end
+    end
 
 end
 
