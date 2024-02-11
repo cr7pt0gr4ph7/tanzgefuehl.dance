d66be72d255c1eb986a962debdc4ce0ecc84684d
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2024-02-11 10:37:59 +0100

    Update seo_tag_patches.rb

--- _plugins/seo_tag_patches.rb
+++ _plugins/seo_tag_patches.rb
@@ -3,7 +3,7 @@ module SeoTagDropExtensions
 
   def page_title
     if page["use_full_page_title"]
-      @page_title ||= site_title
+      @page_title ||= title
     else
       @page_title ||= format_string(page["title"]) || site_title
     end
@@ -11,11 +11,11 @@ module SeoTagDropExtensions
 
   def title
     @title ||= begin
-      if site_title && format_string(page["title"]) && !page["use_full_page_title"]
+      if site_title && format_string(page["title"])
         format_string(page["title"]) + TITLE_SEPARATOR + site_title
       elsif site_description && site_title
         site_title + TITLE_SEPARATOR + site_tagline_or_description
-      elsif !page["use_full_page_title"]
+      elsif
         format_string(page["title"]) || site_title
       else
         site_title
